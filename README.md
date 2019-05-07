ac3e8fe0787d42d3574778aea778f46e58b02373
Leandro Santiago Gomes <leandroluk@gmail.com>
2019-05-07 00:30:11 -0300

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-# web-search-engine-js
-Projeto de sistema 
---
+++ models.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+// Constants
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+
+const PAGE = 'Page';
+const CONTENT = 'Content';
+
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+// Models
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+
+const pageModel = new mongoose.model(PAGE, new mongoose.Schema({
+  title: {
+    type: String,
+    index: true,
+  },
+  link: {
+    type: String,
+    index: true,
+  },
+  contents: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: CONTENT
+  }
+}));
+
+const contentModel = new mongoose.model(CONTENT, new mongoose.Schema({
+  word: {
+    type: String,
+    index: true,
+  },
+  pages: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: PAGE
+  }
+}));
+
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+// Methods
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+
+module.exports = {
+  Page: pageModel,
+  Content: contentModel
+}
